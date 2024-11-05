9ff92ad61b6fa23d98a51451c83b170d080ebd65
Juan Diego de la Ossa <delaossajuan18@Gmail.com>
2024-11-05 14:52:54 -0500

    primer commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { PrincipalComponent } from './estructura/principal.component';
+import { DashboardComponent } from './modulos/dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "", component: PrincipalComponent,
+        children:
+        [
+            {path: "dashboar", component: DashboardComponent},
+            {path: "", redirectTo: "dashboard", pathMatch:"full"}
+        ]
+
+    }
+];
